3cc4a974f95df8627c04021bb7808900c9c31932
ranjan <ranjan.sahoo@girnarsoft.com>
2025-11-03 13:03:21 +0530

    chore(next): move typedRoutes out of experimental

--- .next/dev/static/chunks/src_app_layout_tsx_b1ea393c._.js
+++ .next/dev/static/chunks/src_app_layout_tsx_b1ea393c._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/src_index_8b6fca74.css",
-  "static/chunks/node_modules_c00da1cc._.js",
-  "static/chunks/src_e07f7b33._.js"
+  "static/chunks/node_modules_d1d9dc91._.js",
+  "static/chunks/src_e18ba9d6._.js"
 ],
     source: "dynamic"
 });
